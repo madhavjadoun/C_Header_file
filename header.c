04b1ab2f7594be51af64417ee4772f16b073587f
madhavjadoun <jadounmadhav44@gmail.com>
2025-04-30 20:02:13 +0530

    Update header.c

--- header.c
+++ header.c
@@ -1,4 +1,69 @@
 #include <stdio.h>
-int main(){
-    printf()
+
+int main() {
+    int num, originalNum, reversedNum = 0, remainder;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+    while (num != 0) {
+        remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+    if (originalNum == reversedNum)
+        printf("%d is a palindrome.\n", originalNum);
+    else
+        printf("%d is not a palindrome.\n", originalNum);
+
+    return 0;
 }
+
+//CODE FOR FINDING A SPY NUMBER;
+#include <stdio.h>
+int main() {
+    int num, digit, sum = 0, product = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    int temp = num;
+    while (temp > 0) {
+        digit = temp % 10;
+        sum += digit;
+        product *= digit;
+        temp /= 10;
+    }
+    if (sum == product)
+        printf("%d is a Spy Number.\n", num);
+    else
+        printf("%d is not a Spy Number.\n", num);
+
+    return 0;
+}
+
+//CODE FOR FINDING AN ARMSTRONG NUMBER:
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int num, originalNum, remainder, n = 0;
+    float result = 0.0;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    originalNum = num;
+    int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        n++;
+    }
+    temp = num;
+    while (temp != 0) {
+        remainder = temp % 10;
+        result += pow(remainder, n);
+        temp /= 10;
+    }
+    if ((int)result == originalNum)
+        printf("%d is an Armstrong number.\n", originalNum);
+    else
+        printf("%d is not an Armstrong number.\n", originalNum);
+
+    return 0;
+}
+
